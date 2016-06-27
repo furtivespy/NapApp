91f578d4c9ba9c43504fde9583e7172ed9ac0e8f
Will <furtivespy@gmail.com>
2016-06-27 12:56:49 -0400

    is the only way to make it work to include the key in the code?
    why you do this webpack?

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,10 +17,10 @@ import configureStore from '../stores/configureStore'
 
 
 var config = {
-			    apiKey: process.env.FIREBASE_API || "SECRETS!",
-			    authDomain: process.env.FIREBASE_DOMAIN ||"napapp-cfaef.firebaseapp.com",
-			    databaseURL: process.env.FIREBASE_DATABASE ||"https://napapp-cfaef.firebaseio.com",
-			    storageBucket: process.env.FIREBASE_BUCKET ||"napapp-cfaef.appspot.com",
+			    apiKey: "AIzaSyD1WQzyOkvWKkkweP_3sjR6OwUO9R9PIAQ",
+			    authDomain:"napapp-cfaef.firebaseapp.com",
+			    databaseURL: "https://napapp-cfaef.firebaseio.com",
+			    storageBucket: "napapp-cfaef.appspot.com",
 			};
 			
 Firebase.initializeApp(config);
